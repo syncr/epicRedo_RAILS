38a414c172e16e61e5846b70e738db8fcb587142
Jeremy Whitaker <email.jwhitaker@gmail.com>
2014-09-05 10:37:45 -0700

    passes test for numericality of number

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,6 @@ class Lesson < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
   validates :number, presence: true, uniqueness: true
+  validates :number, numericality: { only_integer: true }
+
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -8,5 +8,7 @@ describe Lesson do
   it { should validate_uniqueness_of :description }
   it { should validate_presence_of :number }
   it { should validate_uniqueness_of :number }
+  it { should validate_numericality_of :number }
+
 
 end
