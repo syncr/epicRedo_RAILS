9191e2488dad3d50591754a3aaa16f497f340ba6
Jeremy Whitaker <email.jwhitaker@gmail.com>
2014-09-05 10:35:58 -0700

    passes test for presence, uniqueness of number

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,5 @@
 class Lesson < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
+  validates :number, presence: true, uniqueness: true
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -6,4 +6,7 @@ describe Lesson do
   it { should validate_uniqueness_of :name }
   it { should validate_presence_of :description }
   it { should validate_uniqueness_of :description }
+  it { should validate_presence_of :number }
+  it { should validate_uniqueness_of :number }
+
 end
