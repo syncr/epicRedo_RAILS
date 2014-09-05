ed665a5c56dd7d4dfa7f330a539c6e95226df76c
Jeremy Whitaker <email.jwhitaker@gmail.com>
2014-09-05 10:34:23 -0700

    passes test for presence, uniqueness of description

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,4 @@
 class Lesson < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-
+  validates :description, presence: true, uniqueness: true
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -4,4 +4,6 @@ describe Lesson do
 
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :name }
+  it { should validate_presence_of :description }
+  it { should validate_uniqueness_of :description }
 end
