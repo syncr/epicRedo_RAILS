d962808017ac31a18c8e68be2e86e0607069de4f
Jeremy <email.jwhitaker@gmail.com>
2014-09-08 14:14:38 -0700

    pass spec for next and prev methods for Lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,15 @@ class Lesson < ActiveRecord::Base
   validates :description, presence: true, uniqueness: true
   validates :number, presence: true, uniqueness: true
   validates :number, numericality: { only_integer: true }
+
+  default_scope { order(number: :asc) }
+
+
+  def next
+    next_lesson = Lesson.where("number > ?", self.number).first
+  end
+
+  def prev
+    prev_lesson = Lesson.where("number < ?", self.number).last
+  end
 end
