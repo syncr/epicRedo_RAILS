975791bff12d681a0be9362e1081e25c7ba0c7ec
Jeremy <email.jwhitaker@gmail.com>
2014-09-08 10:17:53 -0700

    add view erb to accomodate list Lessons method

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,20 +1,20 @@
  class LessonsController < ApplicationController
 
   def index
-    lessons = Lesson.all
+    @lessons = Lesson.all
     render("lessons/index.html.erb")
   end
 
   def new
-    lesson = Lesson.new
-    lessons = Lesson.all
+    @lesson = Lesson.new
+    @lessons = Lesson.all
     render("lessons/new.html.erb")
   end
 
   def create
-    lesson = Lesson.new(params[:lesson])
-    lessons = Lesson.all
-    if lesson.save
+    @lesson = Lesson.new(params[:lesson])
+    @lessons = Lesson.all
+    if @lesson.save
       flash[:notice] = "Your lesson has been created."
       redirect_to('/lessons/new')
     else
@@ -23,7 +23,7 @@
   end
 
   def show
-    lesson = Lesson.find(params[:id])
+    @lesson = Lesson.find(params[:id])
     render("lessons/show.html.erb")
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   match('lessons', {:via => :get, :to => 'lessons#index'})
   match('lessons/new', {:via => :get, :to => 'lessons#new'})
   match('lessons', {:via => :post, :to => 'lessons#create'})
+  match('lessons/:id', {:via => :get, :to => 'lessons#show'})
 end
