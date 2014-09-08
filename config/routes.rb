cca350e38ea77ffa797e6a15376807e50f771946
Jeremy <email.jwhitaker@gmail.com>
2014-09-08 10:45:33 -0700

    add edit method and view for Lessons

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   match('lessons/new', {:via => :get, :to => 'lessons#new'})
   match('lessons', {:via => :post, :to => 'lessons#create'})
   match('lessons/:id', {:via => :get, :to => 'lessons#show'})
+  match('lessons/:id/edit', {:via => :get, :to => 'lessons#edit'})
 end
