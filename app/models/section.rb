fe4a3d3be3026b635d9d459945b4e020cee623ec
Jeremy <email.jwhitaker@gmail.com>
2014-09-09 20:47:42 -0700

    modify list lessons to sorted list

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,8 @@
 class Section < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-
+  validates :number, presence: true, uniqueness: true
+  validates :number, numericality: { only_integer: true }
   has_many :lessons
-
+  
+  default_scope { order(number: :asc) }
 end
