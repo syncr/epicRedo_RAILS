a350a66286f775933e7f96d414eb5a3526cc59ea
Jeremy <email.jwhitaker@gmail.com>
2014-09-09 20:50:56 -0700

    modify lessons table, column description to accomodate text blocks over 255 characters

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910032942) do
+ActiveRecord::Schema.define(version: 20140910034804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lessons", force: true do |t|
     t.string  "name"
-    t.string  "description"
+    t.text    "description"
     t.integer "number"
     t.integer "section_id"
   end
